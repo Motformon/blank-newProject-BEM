ba81641b2d52d34e893c10149d1eeadf7791272e
Motformon <Motformon@gmail.com>
2018-08-09 10:55:02 +0300

    plus scroll

--- app/js/common.js
+++ app/js/common.js
@@ -18,3 +18,38 @@ $(function() {
 	});
 
 });
+
+
+$(window).scroll(function () {
+	if ($(this).scrollTop() > $(this).height()) {
+		$('.top-scroll').addClass('top-scroll_active');
+	} else {
+		$('.top-scroll').removeClass('top-scroll_active');
+	}
+	$('.top-scroll').click(function () {
+		$("html, body").stop().animate({ scrollTop: 0 }, 'slow', 'swing');
+	});
+
+});
+
+
+function sendMail() {
+	$("#order-form").submit(function () {
+		$.ajax({
+			type: "POST",
+			url: "../ajax/mail-form_order.php",
+			data: $(this).serialize(),
+			success: function (response) {
+				if (response) {
+					console.log("send");
+					$(".popup-thank").show("500");
+				} else {
+					console.log("Ошибка отправки");
+				}
+			},
+			error: function (response) {
+				console.log("Файл не найден");
+			}
+		});
+	});
+}
